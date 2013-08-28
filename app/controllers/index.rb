420007a5a25176b8f74573df406da7841709cafb
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-28 10:00:45 -0500

    Create new link

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,4 @@
+
 get '/' do
   # Look in app/views/index.erb
   erb :index
@@ -5,5 +6,9 @@ end
 
 post '/urls' do
   @long_url = params[:long_url]
+  @key = Link.build_key
+  p @key
+  params[:key] = @key
+  new_link = Link.create(params)
   erb :shorten_url_TEST
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,15 +1,14 @@
 class Link < ActiveRecord::Base
-  validates :long_url, presence: true
-  validates :key, uniqueness: true
-  validates :counter, presence: true
+  # validates :long_url, presence: true
+  # validates :key, uniqueness: true
+  # validates :counter, presence: true
 
   def redirect_counter
     
   end
 
-  letters_numbers = ("A".."Z").to_a + ("a".."z").to_a + ("1".."9").to_a
-
-  def build_key
+  def self.build_key
+    letters_numbers = ("A".."Z").to_a + ("a".."z").to_a + ("1".."9").to_a
     key = ''
     8.times { key << letters_numbers.sample }
     key
