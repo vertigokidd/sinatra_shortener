0d0406bec82ad7400786b295aaa850346074ee43
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-28 09:46:23 -0500

    Working on model

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,15 +1,20 @@
 class Link < ActiveRecord::Base
   validates :long_url, presence: true
-  validates :short_url, uniqueness: true
+  validates :key, uniqueness: true
   validates :counter, presence: true
-
-  def short_url_generator
-    
-  end
 
   def redirect_counter
     
   end
+
+  letters_numbers = ("A".."Z").to_a + ("a".."z").to_a + ("1".."9").to_a
+
+  def build_key
+    key = ''
+    8.times { key << letters_numbers.sample }
+    key
+  end
+
 end
 
 
--- db/migrate/20130828091049_create_links.rb
+++ db/migrate/20130828091049_create_links.rb
@@ -2,9 +2,10 @@ class CreateLinks < ActiveRecord::Migration
   def change
     create_table :links do |t|
       t.string :long_url
-      t.string :short_url
+      t.string :key
       t.string :counter
 
       t.timestamps
+    end
   end
 end
