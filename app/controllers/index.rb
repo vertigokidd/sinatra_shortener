2f21f42f19f456911943c49d5179bced6f4ae40a
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-28 10:54:51 -0500

    Working MVP

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,14 +1,22 @@
+#GET =========================================
 
 get '/' do
   # Look in app/views/index.erb
   erb :index
 end
 
+get '/:short_url' do
+  @key = params[:short_url]
+  link = Link.find_by_key("#{@key}").long_url
+  redirect "http://#{link}"
+end
+
+#POST ========================================
+
 post '/urls' do
   @long_url = params[:long_url]
   @key = Link.build_key
-  p @key
   params[:key] = @key
   new_link = Link.create(params)
-  erb :shorten_url_TEST
+  erb :shorten_url
 end
